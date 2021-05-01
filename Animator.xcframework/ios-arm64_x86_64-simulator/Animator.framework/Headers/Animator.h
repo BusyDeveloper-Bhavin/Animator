4b89cd48257fc6db40a8b0f477156cad1322007e
Bhavin <bhavinv@hyperlinkinfosystem.net.in>
2021-05-01 23:04:49 +0530

    Framework Added

---
+++ Animator.xcframework/ios-arm64_x86_64-simulator/Animator.framework/Headers/Animator.h
@@ -0,0 +1,18 @@
+//
+//  Animator.h
+//  Animator
+//
+//  Created by Bhavin on 01/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Animator.
+FOUNDATION_EXPORT double AnimatorVersionNumber;
+
+//! Project version string for Animator.
+FOUNDATION_EXPORT const unsigned char AnimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animator/PublicHeader.h>
+
+
